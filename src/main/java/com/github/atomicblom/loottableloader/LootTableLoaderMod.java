0f81f0d0e5af19b920969cec6d713d70a84dba20
Steven Blom <atomicblom@binaryvibrance.net>
2017-06-27 21:43:33 +0930

    Update Build

--- src/main/java/com/github/atomicblom/loottableloader/LootTableLoaderMod.java
+++ src/main/java/com/github/atomicblom/loottableloader/LootTableLoaderMod.java
@@ -4,13 +4,14 @@ import com.google.common.io.Files;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.common.network.NetworkCheckHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -18,36 +19,47 @@ import java.util.Map;
 
 import static com.github.atomicblom.loottableloader.LootTableLoaderMod.MOD_ID;
 import static com.github.atomicblom.loottableloader.LootTableLoaderMod.MOD_NAME;
+import static com.github.atomicblom.loottableloader.LootTableLoaderMod.MOD_VERSION;
 
-@Mod(modid = MOD_ID, name = MOD_NAME)
+@SuppressWarnings({"MethodMayBeStatic", "ObjectAllocationInLoop", "ClassHasNoToStringMethod"})
+@Mod(modid = MOD_ID, name = MOD_NAME, version = MOD_VERSION)
 public class LootTableLoaderMod {
 
-    private org.apache.logging.log4j.Logger logger = LogManager.getLogger(LootTableLoaderMod.MOD_ID);
+    private final Logger logger = LogManager.getLogger(LootTableLoaderMod.MOD_ID);
 
-    public static final String MOD_ID = "loot_table_loader";
-    public static final String MOD_NAME = "Loot Table Loader";
-    public static File configDir;
-    public static final FilenameFilter jsonFilter = (File dir, String name) -> name.toLowerCase().endsWith(".json");
+    static final String MOD_ID = "loot_table_loader";
+    static final String MOD_NAME = "Loot Table Loader";
+    static final String MOD_VERSION = "@MOD_VERSION@";
 
-    @Mod.EventHandler
+    private static File configDir;
+    private static final FilenameFilter jsonFilter = (File dir, String name) -> name.toLowerCase().endsWith(".json");
+
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        File rootConfigDir = event.getModConfigurationDirectory();
+        final File rootConfigDir = event.getModConfigurationDirectory();
 
         configDir = new File(rootConfigDir, "loot_table_loader");
         if (!configDir.exists()) {
             logger.info("Loot table config directory does not exist, creating");
-            configDir.mkdir();
+            if (!configDir.mkdir()) {
+                logger.error("Could not create Loot Table Manager configuration loot directory");
+            }
         }
 
+        final File[] files = configDir.listFiles(jsonFilter);
+        if (files == null || files.length == 0) {
+            logger.info("No files found in the Loot table config directory.");
+            return;
+        }
         int addedResources = 0;
-        for (File file : configDir.listFiles(jsonFilter)) {
-            String fileName = file.getName();
-            int i = fileName.lastIndexOf(".");
-            String resourceName = fileName.substring(0, i);
+        for (final File file : files) {
+            final String fileName = file.getName();
+            final int i = fileName.lastIndexOf('.');
+            final String resourceName = fileName.substring(0, i);
             LootTableList.register(new ResourceLocation(MOD_ID, resourceName));
             addedResources++;
         }
-        logger.info("Added %d additional loot tables", addedResources);
+        logger.info(String.format("Added %d additional loot tables", addedResources));
     }
 
     @NetworkCheckHandler
@@ -55,13 +67,13 @@ public class LootTableLoaderMod {
         return true;
     }
 
-    @Mod.EventHandler
+    @EventHandler
     public void onServerAboutToStart(FMLServerAboutToStartEvent event) {
-        String folderName = event.getServer().getFolderName();
-        File worldDirectory = event.getServer().getActiveAnvilConverter().getSaveLoader(folderName, false).getWorldDirectory();
+        final String folderName = event.getServer().getFolderName();
+        final File worldDirectory = event.getServer().getActiveAnvilConverter().getSaveLoader(folderName, false).getWorldDirectory();
 
-        File lootTableDirectory = new File(new File(worldDirectory, "data"), "loot_tables");
-        File ltmDirectory = new File(lootTableDirectory, LootTableLoaderMod.MOD_ID);
+        final File lootTableDirectory = new File(new File(worldDirectory, "data"), "loot_tables");
+        final File ltmDirectory = new File(lootTableDirectory, MOD_ID);
         if (!ltmDirectory.exists()) {
             logger.info("Creating loot table directory in world for mod");
             if (!ltmDirectory.mkdirs()) {
@@ -69,16 +81,21 @@ public class LootTableLoaderMod {
             }
         }
 
+        final File[] files = configDir.listFiles(jsonFilter);
+        if (files == null || files.length == 0) {
+            logger.info("No files found in the Loot table config directory.");
+            return;
+        }
         int copiedLootTables = 0;
-        for (File file : LootTableLoaderMod.configDir.listFiles(LootTableLoaderMod.jsonFilter)) {
-            String fileName = file.getName();
+        for (final File file : files) {
+            final String fileName = file.getName();
             try {
                 Files.copy(file, new File(ltmDirectory, fileName));
                 copiedLootTables++;
-            } catch (IOException e) {
-                logger.error("Unable to copy %s to world loot directory", fileName);
+            } catch (final IOException e) {
+                logger.error(String.format("Unable to copy %s to world loot directory", fileName));
             }
         }
-        logger.info("Copied %d loot tables", copiedLootTables);
+        logger.info(String.format("Copied %d loot tables", copiedLootTables));
     }
 }
